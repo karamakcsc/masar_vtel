bb23a819ef696d2a50cffca18baf41a66d466b07
karamakcsc <karamakcsc@hotmail.com>
2026-06-25 13:16:20 +0300

    update 25-06-2026

---
+++ masar_vtel/masar_vtel/doctype/vtel_transaction/test_vtel_transaction.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, KCSC and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVTELTransaction(FrappeTestCase):
+	pass
---
+++ masar_vtel/masar_vtel/doctype/vtel_transaction/vtel_transaction.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, KCSC and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("VTEL Transaction", {
+// 	refresh(frm) {
+
+// 	},
+// });
